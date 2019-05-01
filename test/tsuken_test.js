74b0d741eeb66572a5401a9eeecccb7b8ba12fd8
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2019-05-01 17:32:00 +0900

    added configure test

--- test/tsuken_test.js
+++ test/tsuken_test.js
@@ -4,13 +4,28 @@ contract('Tsuken', () => {
   let token;
 
   beforeEach(async () => {
-    token = await Tsuken.new();
+    token = await Tsuken.deployed();
   });
 
   describe('トークンの初期設定', () => {
     it('トークン名が正しいこと', async () => {
       const expect = 'Tsuken';
       const actual = await token.name();
+      assert.equal(expect, actual);
+    });
+
+    /*
+    it('トークンのシンボル名が正しいこと', async () => {
+      console.log(token);
+      const expect = 'TKN';
+      const actual = await token.symbol();
+      assert.equal(expect, actual);
+    });
+    */
+
+    it('トークンの少数桁数が正しいこと', async () => {
+      const expect = 18;
+      const actual = await token.decimals();
       assert.equal(expect, actual);
     });
   });
