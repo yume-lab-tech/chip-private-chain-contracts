ade7d689f3bab51abdd51cad705cd5e7cbab6e13
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2019-05-01 15:21:53 +0900

    fix by solium

--- contracts/Tsuken.sol
+++ contracts/Tsuken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Tsuken is ERC20 {
-    string public name = 'Tsuken';
-    string public symbol = 'TKN';
+    string public name = "Tsuken";
+    string public symbol = "TKN";
 
     // same as ether. (1ether=1wei * (10 ** 18))
     uint public decimals = 18;
