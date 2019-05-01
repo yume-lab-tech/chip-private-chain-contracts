889043f6a7b6a616548d2fb98644d7a2fe010900
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2019-05-01 13:48:10 +0900

    added development network settings.

--- truffle-config.js
+++ truffle-config.js
@@ -36,6 +36,12 @@ module.exports = {
    */
 
   networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 4600000
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
