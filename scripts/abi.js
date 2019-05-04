1f371c7fce82e6db966f5aa81738ef97b13f51f2
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2019-05-04 17:44:23 +0900

    lintルール修正

--- scripts/abi.js
+++ scripts/abi.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 const contract = JSON.parse(fs.readFileSync('./build/contracts/Tsuken.json', 'utf8'));
 
 console.log(JSON.stringify(contract.abi));
--- scripts/blockNumberi.js
+++ scripts/blockNumberi.js
@@ -7,6 +7,7 @@ const targetIpAddress = process.argv[2];
 
 console.log(targetIpAddress);
 
-const W3 = require('web3')
-const web3 = new W3(new W3.providers.HttpProvider('http://' + targetIpAddress + ':8545'))
-web3.eth.getBlockNumber().then(result => {console.log(result)})
+const W3 = require('web3');
+
+const web3 = new W3(new W3.providers.HttpProvider(`http://${targetIpAddress}:8545`));
+web3.eth.getBlockNumber().then((result) => { console.log(result); });
